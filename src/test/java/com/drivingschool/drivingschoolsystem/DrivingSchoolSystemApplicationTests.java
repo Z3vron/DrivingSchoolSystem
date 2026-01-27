dc9e76cfc35f958dfa9af66559fceda29ff66aac
Andrzej <E-mail@>
2026-01-27 13:57:46 +0100

    First commit - local Health check and docker

---
+++ src/test/java/com/drivingschool/drivingschoolsystem/DrivingSchoolSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.drivingschool.drivingschoolsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrivingSchoolSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
